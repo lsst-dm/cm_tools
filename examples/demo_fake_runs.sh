ab77648acf38bd43dfef74f8a12febbb8d21da39
Eric Charles <badass@stanford.edu>
2022-07-27 17:27:35 -0700

    udpated demo

--- examples/demo_fake_runs.sh
+++ examples/demo_fake_runs.sh
@@ -3,3 +3,7 @@ c_name="test"
 command="cm"
 
 ${command} fake_run --level campaign --production_name ${p_name} --campaign_name ${c_name}
+${command} print_table --table campaign
+${command} print_table --table step
+${command} print_table --table group
+${command} print_table --table workflow
--- examples/demo_server.sh
+++ examples/demo_server.sh
@@ -1,5 +1,6 @@
 \rm cm.db
 \rm -rf archive_test
+
 p_name="example"
 c_name="test"
 handler="lsst.cm.tools.example.handler.ExampleHandler"
@@ -7,6 +8,6 @@ config="examples/example_config.yaml"
 command="cm"
 
 ${command} create
-${command} insert --level production --production_name ${p_name} --handler ${handler} --config_yaml ${config}
-${command} insert --recurse --level campaign --production_name ${p_name} --campaign_name ${c_name} --handler ${handler} --config_yaml ${config}
+${command} insert --level production --production_name ${p_name}
+${command} insert --level campaign --production_name ${p_name} --campaign_name ${c_name} --handler ${handler} --config_yaml ${config}
 ${command} daemon --production_name ${p_name} --campaign_name ${c_name} &
