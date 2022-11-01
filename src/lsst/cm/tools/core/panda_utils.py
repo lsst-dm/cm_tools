85077a5f7b3c35b9cde4de5dda974e4ea97322a3
Sierra Villarreal <sierrav@slac.stanford.edu>
2022-11-01 15:55:53 -0500

    rebuilding around panda job level error codes

--- src/lsst/cm/tools/core/panda_utils.py
+++ src/lsst/cm/tools/core/panda_utils.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from pandaclient import panda_api
+from pandaclient import Client, panda_api
 
 from lsst.cm.tools.core.db_interface import JobBase
 from lsst.cm.tools.core.slurm_utils import SlurmChecker
@@ -39,26 +39,118 @@ def parse_bps_stdout(url: str) -> dict[str, str]:
     return out_dict
 
 
-def check_panda_status(panda_url: str, panda_username=None) -> list[str]:
-    """Check the status of a panda job
+def get_jeditaskid_from_reqid(reqid: int, username: str) -> list[int]:
+    """Return the jeditaskids associated with a reqid.
 
     Parameters
     ----------
-    panda_url: str
-        typically a reqid associated with the job
-    panda_username: str
-        None by default, username required for other submitters
+    reqid: int
+        PanDA reqid as reported to bps submit
+    username: str
+        Username of original submitter
 
     Returns
     -------
-    job_statuses: list[str]
-        list of status messages with associated task_id
+    jeditaskids: list[int]
+        A list of all jeditaskIDs associated with the
+        submitted reqid
     """
+    # TODO: try to find a way to do this with Client to avoid the
+    # requirement on username storage
     conn = check_panda_conn()
-    tasks = conn.get_tasks(task_ids=panda_url, username=panda_username)
-    job_statuses = [task["status"] for task in tasks]
+    reqid_pull = conn.get_tasks(task_ids=reqid, username=username)
+    jeditaskids = [reqid["jeditaskid"] for reqid in reqid_pull]
 
-    return job_statuses
+    return jeditaskids
+
+
+def get_errors_from_jeditaskid(jeditaskid: int):
+    """Return the errors associated with a jeditaskid
+
+    Parameters
+    ----------
+    jeditaskid: int
+        A jeditaskid, which will have some number of pandaIDs associated.
+
+    Returns
+    -------
+    error_codes: list[dict]
+        A list of dictionaries matching error code category
+        to the returned value.
+    error_diags: list[dict]
+        A list of dictionaries matching error code categories
+        to the associated diagnostic messages.
+    """
+    conn_status, task_status = Client.getJediTaskDetails({"jediTaskID": jeditaskid}, True, True)
+
+    # grab all the PanDA IDs
+    if conn_status == 0:
+        job_ids = list(task_status["PandaID"])
+        jobs_list = []
+        if len(job_ids) > 0:
+            chunksize = 2000  # max number of allowed connections to PanDA
+            chunks = [job_ids[i : i + chunksize] for i in range(0, len(job_ids), chunksize)]
+            for chunk in chunks:
+                conn_status, ret_jobs = Client.getFullJobStatus(ids=chunk, verbose=False)
+                if conn_status == 0:
+                    jobs_list.extend(ret_jobs)
+        elif len(job_ids) == 1:
+            conn_status, ret_jobs = Client.getFullJobStatus(ids=job_ids, verbose=False)
+            if conn_status == 0:
+                jobs_list = ret_jobs
+        else:
+            print("no jobs found")
+            return [], []
+            # TODO: properly address this break condition,
+            # because something went wrong
+
+    # now we need to parse all the error codes for failed PandaIDs
+    errors_all = []
+    diags_all = []
+
+    failed_jobs = [job for job in jobs_list if job.jobStatus == "failed"]
+    if len(failed_jobs) == 0:
+        return (errors_all, diags_all)
+    else:
+        for job in failed_jobs:
+            errors = dict()
+            diags = dict()
+
+            # brokerageErrorCode/Diag
+            if job.brokerageErrorCode != 0:
+                errors["brokerage"] = job.brokerageErrorCode
+                diags["brokerage"] = job.brokerageErrorDiag
+            # ddmErrorCode/Diag
+            if job.ddmErrorCode != 0:
+                errors["ddm"] = job.ddmErrorCode
+                diags["ddm"] = job.ddmErrorDiag
+            # exeErrorCode/Diag
+            if job.exeErrorCode != 0:
+                errors["exe"] = job.exeErrorCode
+                diags["exe"] = job.exeErrorDiag
+            # jobDispatcherErrorCode/Diag
+            if job.jobDispatcherErrorCode != 0:
+                errors["jobDispatcher"] = job.jobDispatcherErrorCode
+                diags["jobDispatcher"] = job.jobDispatcherErrorDiag
+            # pilotErrorCode/Diag
+            if job.pilotErrorCode != 0:
+                errors["pilot"] = job.pilotErrorCode
+                diags["pilot"] = job.pilotErrorDiag
+            # supErrorCode/Diag
+            if job.supErrorCode != 0:
+                errors["sup"] = job.supErrorCode
+                diags["sup"] = job.supErrorDiag
+            # taskBufferErrorCode/Diag
+            if job.taskBufferErrorCode != 0:
+                errors["taskBuffer"] = job.taskBufferErrorCode
+                diags["taskBuffer"] = job.taskBufferErrorDiag
+            # transExitCode (no Diag)
+            if job.transExitCode != 0:
+                errors["trans"] = job.transExitCode
+                diags["trans"] = "check the logs"
+            errors_all.append(errors)
+            diags_all.append(diags)
+        return (errors_all, diags_all)
 
 
 class PandaChecker(SlurmChecker):  # pragma: no cover
@@ -80,10 +172,10 @@ class PandaChecker(SlurmChecker):  # pragma: no cover
                 update_vals["panda_url"] = panda_url
         if panda_url is None:
             return update_vals
-        panda_status = check_panda_status(panda_url)
-        if panda_status != job.panda_status:
-            update_vals["panda_status"] = panda_status
-        status = self.panda_status_map[panda_status]
-        if status != job.status:
-            update_vals["status"] = status
+        # panda_status = check_panda_status(panda_url)
+        # if panda_status != job.panda_status:
+        #    update_vals["panda_status"] = panda_status
+        # status = self.panda_status_map[panda_status]
+        # if status != job.status:
+        #    update_vals["status"] = status
         return update_vals
