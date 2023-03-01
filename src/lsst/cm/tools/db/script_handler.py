fdd497adc43da8e993fc940fb47f68c83244f4a2
Eric Charles <echarles@gmail.com>
2023-03-01 08:11:04 -0800

    Changes to handle collection management as per what colin wants

--- src/lsst/cm/tools/db/script_handler.py
+++ src/lsst/cm/tools/db/script_handler.py
@@ -186,7 +186,8 @@ class CollectStepScriptHandler(ScriptHandler):
 
     def write_script_hook(self, dbi: DbInterface, parent: Any, script: ScriptBase, **kwargs: Any) -> None:
         input_colls = [child.coll_out for child in parent.jobs_ if not child.superseded]
-        command = make_butler_associate_command(parent.butler_repo, parent.coll_out, input_colls, None)
+        input_colls.append(parent.coll_in)
+        command = make_butler_chain_command(parent.butler_repo, parent.coll_out, input_colls)
         write_command_script(script, command, **kwargs)
 
     def get_coll_out_name(self, parent: Any, **kwargs: Any) -> str:
